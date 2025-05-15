6244614f8eaa0afdaedbad0a4df85eb850ddef10
boymirzavahabov92@gmail.com <boymirzavahabov92@gmail.com>
2025-05-15 07:21:51 +0500

    Popular cardi tugatildi @media qiloish kerak

--- src/app/components/header/newbies/page.js
+++ src/app/components/header/newbies/page.js
@@ -11,11 +11,15 @@ const Newbies = styled.div`
   width: 70%;
   height: 50rem;
   border: 3px solid red;
-  margin-top: 20rem;
+  margin-top: 21rem;
   margin-left: 15%;
 `;
 
-const Messages = styled.div``;
+const Messages = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1.5rem;
+`;
 
 const StyledCar = styled.div`
   display: flex;
@@ -53,7 +57,7 @@ const StyledCar = styled.div`
 
   .carImage {
     width: 16rem;
-    margin-left: 15.7rem;
+    margin-left: 16rem;
     margin-top: -18.3rem;
     border-radius: 1rem;
   }
@@ -101,7 +105,93 @@ const MessageText = styled.div`
   }
 `;
 
-const Popular = styled.div``;
+const Popular = styled.div`
+  width: 16.4rem;
+  height: 23rem;
+  background-color: #fff;
+  background-image: url("/popular.png");
+  border-radius: 1rem;
+  padding: 1rem;
+
+  .popular {
+    width: 5rem;
+    text-align: center;
+    font-size: 0.9rem;
+    background-color: #3dc47e;
+    color: cef0de;
+    border-radius: 0.2rem;
+    letter-spacing: 0.3px;
+  }
+
+  .art {
+    font-size: 0.8rem;
+    color: rgb(211, 222, 198);
+    margin-top: 7rem;
+    letter-spacing: 0.3px;
+  }
+
+  h4 {
+    font-size: 1rem;
+    line-height: 1.4;
+    color: #fff;
+    letter-spacing: 0.5px;
+  }
+`;
+
+const Price = styled.div`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+  width: 100%;
+  height: 4rem;
+
+  .price {
+    width: 2.5rem;
+    border-radius: 50%;
+    // margin-left: -5rem;
+  }
+`;
+const Like = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 10rem;
+  height: 2rem;
+  gap: 0.5rem;
+
+  .name {
+    width: 12rem;
+    height: 1rem;
+    font-size: 0.8rem;
+    letter-spacing: 0.5px;
+    color: rgb(211, 222, 198);
+    margin-top: 0.01rem;
+  }
+    p{
+      font-size: 0.6rem;
+      color: rgb(145, 147, 142);
+      margin-top: -1rem;
+    }
+
+  .message {
+    width: 1rem;
+    height: 0.9rem;
+    background-color: rgb(130, 134, 125);
+    border-radius: 3px;
+    margin-top: -1rem;
+
+  }
+  p {
+    font-size: 0.6rem;
+  }
+
+  .like {
+    width: 1rem;
+    height: 1.2rem;
+    margin-top: -1.1rem;
+
+  }
+`;
 
 const MessagTwo = styled.div``;
 
@@ -135,15 +225,35 @@ const New = () => {
           <MessageText>
             <img src="/avatarka.png" alt="Avatarka" className="avatar" />
             <h4>
-              Jessica Miller: 
-              <span> Even as the ride-hailing services future remem… &nbsp;</span>
-            </h4> {" "}
-            <img src="/sms.png" alt="Sms" className="smsicon" />
-            {" "}
+              Jessica Miller:
+              <span>
+                {" "}
+                Even as the ride-hailing services future remem… &nbsp;
+              </span>
+            </h4>{" "}
+            <img src="/sms.png" alt="Sms" className="smsicon" />{" "}
             <p className="sms">&nbsp;342</p>
           </MessageText>
         </StyledCar>
-        <Popular></Popular>
+        <Popular>
+          <p className="popular">Popular</p>
+          <p className="art">ART & DESIGN</p>
+          <h4>
+            INVISIBLE INK: THE <br /> WEIRD WORLD OF <br />
+            TATTOO REMOVAL - <br /> IN PICTURES
+          </h4>
+          <Price>
+            <img src="/avatarka.png" alt="Avatarka" className="price" />
+            <Like>
+              <p className="name">By Sarah Jencins</p>
+              <p>Sep 26 &nbsp;</p>
+              <img src="/sms1.png" alt="sms" className="message" />
+              <p>16 &nbsp;</p>&nbsp;
+              <img src="/ola.png" alt="Like" className="like" />
+              <p>832</p>
+            </Like>
+          </Price>
+        </Popular>
       </Messages>
       <MessagTwo>
         <Cats></Cats>
